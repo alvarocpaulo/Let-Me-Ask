cdf929697ba5f9162880f2b18aea3ef13d00d205
Álvaro Costa Paulo <acptdb@gmail.com>
2021-06-22 20:45:54 -0300

    continue aula 2

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { useContext } from 'react';
 import { useHistory } from 'react-router-dom';
 import { auth, firebase } from '../services/firebase';
 
@@ -7,6 +8,7 @@ import googleIconImg from '../assets/images/google-icon.svg'
 
 import '../styles/auth.scss';
 import { Button } from '../components/Button';
+
 
 export function Home(){
     const history = useHistory();
